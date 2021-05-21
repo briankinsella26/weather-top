107fe48dc7ae96c26bdaf088726d0af63da00499
Brian Kinsella <brian.kinsella@distilled.ie>
2021-05-21 12:30:31 +0100

    renamed home page

---
+++ app/controllers/Home.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import models.Member;
+import play.Logger;
+import play.mvc.Controller;
+
+public class Home extends Controller {
+  public static void index() {
+    Logger.info("Rendering Home");
+    Member member = null;
+    if(session.contains("logged_in_memberId")) {
+      String memberId = session.get("logged_in_memberId");
+      member = Member.findById(Long.parseLong(memberId));
+    }
+    render("home.html", member);
+  }
+}
