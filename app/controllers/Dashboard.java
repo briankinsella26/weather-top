f4ea72311043bb1ffe4053c1747a597f6cfe1ca8
Brian Kinsella <brian.kinsella@distilled.ie>
2021-05-21 15:14:54 +0100

    added alphabetical sort for stations

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.Comparator;
 import java.util.List;
 import models.Member;
 import models.Station;
@@ -13,14 +14,17 @@ public class Dashboard extends Controller {
     Logger.info("Rendering Dashboard");
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
+    stations.sort(Comparator.comparing(station -> station.name, String.CASE_INSENSITIVE_ORDER));
 
     for(Station station: stations) {
-      station.latestReading = StationAnalytics.getLatestReading(station.readings);
-      Conversions.performConversions(station.latestReading);
-      Conversions.setMinMaxValues(station);
-      station.temperatureTrend = StationAnalytics.getWeatherTrendIcon(station.readings, "temperature");
-      station.windSpeedTrend = StationAnalytics.getWeatherTrendIcon(station.readings, "windSpeed");
-      station.pressureTrend = StationAnalytics.getWeatherTrendIcon(station.readings, "pressure");
+      if(station.readings.size() > 0) {
+        station.latestReading = StationAnalytics.getLatestReading(station.readings);
+        Conversions.performConversions(station.latestReading);
+        Conversions.setMinMaxValues(station);
+        station.temperatureTrend = StationAnalytics.getWeatherTrendIcon(station.readings, "temperature");
+        station.windSpeedTrend = StationAnalytics.getWeatherTrendIcon(station.readings, "windSpeed");
+        station.pressureTrend = StationAnalytics.getWeatherTrendIcon(station.readings, "pressure");
+        }
     }
 
     render("dashboard.html", member, stations);
@@ -29,7 +33,6 @@ public class Dashboard extends Controller {
   public static void deleteStation(Long id, Long stationId) {
     Logger.info ("Removing station");
     Member member = Accounts.getLoggedInMember();
-    List<Station> stations = Station.findAll();
     Station station = Station.findById(stationId);
     member.stations.remove(station);
     member.save();
@@ -41,7 +44,6 @@ public class Dashboard extends Controller {
   public static void addStation(String name, double latitude, double longitude) {
     Logger.info ("Adding station: " + name);
     Member member = Accounts.getLoggedInMember();
-    List<Station> stations = Station.findAll();
     Station station = new Station(name, latitude, longitude);
     member.stations.add(station);
     station.save();
